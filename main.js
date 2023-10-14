190cd15a528fef3a1588044eb5381a3828d2214d
F2RUZ <gaffarovferuz4@gmail.com>
2023-10-14 21:56:05 +0500

    javaScript2-dars

---
+++ main.js
@@ -0,0 +1,63 @@
+// var name = "Salom";
+
+// var age = 20;
+
+// console.log(name);
+// console.log(age);
+
+// let name = "Samandar";
+// let age = 20;
+
+// console.log(name);
+// console.log(age);
+
+// const name = "Samandar";
+// console.log(name);
+
+// console.log(name);
+// var name = "Samandar";
+// let name = "Samandar";
+
+// console.log(name);
+
+// const name = "Nozim";
+
+// let name = 'Samandar'
+// let age = 20
+// let isMarried = true
+
+// let name = "Samandar",
+//   age = 20,
+//   isMarried = false;
+
+// console.log(name , age , isMarried);
+
+// NUMBER TYPE
+
+// let age = 20;   //raqam
+
+//STRING TYPE
+
+// let name = '20'
+
+// BOOLEAN  TYPE
+
+// let isMarried = false  // yolg'on
+// let isMarried = true   Rost
+
+//  let age = null
+
+// let name;
+// const age
+
+// console.log(name);
+
+// let name = "Samandar";
+// const age = 20;
+
+// alert("Ismi:" + name + "Yoshi :" + age);
+
+// let name = prompt("Ismingiz nima");
+// let age = prompt("Yoshingizni kiriting ");
+
+// console.log(name, age);
